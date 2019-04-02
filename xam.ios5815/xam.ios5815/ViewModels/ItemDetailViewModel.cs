a10cdd52b23b9d937d1defc7262d55f43546b53b
LGFox <lgfox@mail.ru>
2019-04-02 12:41:55 +0300

    Added a test project for https://github.com/xamarin/xamarin-macios/issues/5815

---
+++ xam.ios5815/xam.ios5815/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using xam.ios5815.Models;
+
+namespace xam.ios5815.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ xam.ios5815/xam.ios5815/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xam.ios5815.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
